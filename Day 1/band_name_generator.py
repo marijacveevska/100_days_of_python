0b21eebb21784a05c9ad8c0f51ba3ad4f469e1ce
Marija Cveevska <marijacveevska@gmail.com>
2025-12-07 21:47:58 +0100

    Day 1 - basic intro

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in? \n")
+pet = input("Which animal is your pet? \n")
+print("The name of your band is: "+ city + " " + pet)
+
