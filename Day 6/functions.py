146bed5ba7772654c11a9cc626770817c0a8ad88
Marija Cveevska <marijacveevska@gmail.com>
2025-12-23 07:51:07 +0100

    Day 6 - functions

---
+++ Day 6/functions.py
@@ -0,0 +1,16 @@
+# Functions are "self contained" modules of code that accomplish a specific task. Functions usually "take in" data, process it, and "return" a result. Once a function is written, it can be used over and over and over again.
+
+# examples
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+# Defining a function
+def my_function():
+    print("Hello")
+    print("Bye")
+
+# Calling a function
+my_function()    
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
