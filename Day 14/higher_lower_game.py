45175eff3cf85db933f181184159795b4a3a4dbc
Marija Cveevska <marijacveevska@gmail.com>
2026-01-05 18:11:16 +0100

    Higher Lower Game, ver.2

--- Day 14/higher_lower_game.py
+++ Day 14/higher_lower_game.py
@@ -1,38 +1,32 @@
-"""
-Work in progress
-"""
 import random
 from higher_lower_art import art
 from game_data import data
 
 print(art)
 
-
+current = random.choice(data)
 
 still_play = True
 score = 0
 while still_play:
 
-    x = random.randint(0,49)
-    y = random.randint(0,49)
-    if y == x :
-        if y == 0:
-            y += 1
-        else:
-            y -=1
+    
+    challenger = random.choice(data)
+    while challenger == current :
+        challenger = random.choice(data)
 
     
-    A_name = data[x]['name']
-    A_description = data[x]['description']
-    A_country = data[x]['country']
-    A_followers = data[x]['follower_count']
+    A_name = current['name']
+    A_description = current['description']
+    A_country = current['country']
+    A_followers = current['follower_count']
     #print(A_followers)
     print(f"A: {A_name}, {A_description} from {A_country}")
 
-    B_name = data[y]['name']
-    B_description = data[y]['description']
-    B_country = data[y]['country']
-    B_followers = data[y]['follower_count']
+    B_name = challenger['name']
+    B_description = challenger['description']
+    B_country = challenger['country']
+    B_followers = challenger['follower_count']
     #print(B_followers)
     print(f"B: {B_name}, {B_description} from {B_country}")
 
@@ -50,7 +44,8 @@ while still_play:
     is_correct = check_answer(A_followers,B_followers,guess)
     if is_correct == True:
         score +=1
+        current = challenger
         print(f"You're right! Current score: {score} \n")
     else:
         still_play = False
-        print(f"Sorry, that's wrong. Your score is {score}")
+        print(f"\nSorry, that's wrong. Your score is {score}\n")
