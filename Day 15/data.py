dfad8cadbc88f70e2ef0d336d47a0fd9afb7df0a
Marija Cveevska <marijacveevska@gmail.com>
2026-01-06 17:09:44 +0100

    Day 15 - data for the Coffee Machine Project

---
+++ Day 15/data.py
@@ -0,0 +1,40 @@
+CURRENCY = "$"
+
+COIN_VALUES = {
+    "quarters": 0.25,
+    "dimes": 0.10,
+    "nickles": 0.05,
+    "pennies": 0.01
+    }
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    }
+
+menu_item = [
+    {
+        "name":"espresso", 
+        "water":50, 
+        "milk":0, 
+        "coffee":18, 
+        "cost":1.5 
+        
+    },
+    {
+        "name":"latte", 
+        "water":200, 
+        "milk":150, 
+        "coffee":24, 
+        "cost":2.5
+    },
+    {
+        "name":"cappuccino", 
+        "water":250, 
+        "milk":50, 
+        "coffee":24, 
+        "cost":3
+    },
+]
+
