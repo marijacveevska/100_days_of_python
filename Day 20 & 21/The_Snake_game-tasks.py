f0220d884adc5f7b9c201a65fc694c757b1f3240
Marija Cveevska <marijacveevska@gmail.com>
2026-01-18 12:17:20 +0100

    Day 20 - starting with the Snake Game

---
+++ Day 20 & 21/The_Snake_game-tasks.py
@@ -0,0 +1,10 @@
+# Day 20
+# TODO-1 - Create snake body
+# TODO-2 - Move the snake (continuously move forward, and we only tell it to change direction)
+# TODO-3 - Control the snake with our up,down,left and right arrow keys
+# Day 21
+# TODO-4 - Detect collision with food and new random piece of food appears randomly on some location
+# TODO-5 - Create a scoreboard in the game screen
+# TODO-6 - Detect collision with wall (End game)
+# TODO-7 - Detect collision with tail (End game)
+
