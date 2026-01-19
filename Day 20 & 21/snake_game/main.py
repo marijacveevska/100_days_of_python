66ece7e3b2a6da08295e3256b6fb10e30627f88a
Marija Cveevska <marija.cveevska@zoncoalitie.nl>
2026-01-19 15:33:15 +0100

    Day 21 - snake game , collision with food and food class

--- Day 20 & 21/snake_game/main.py
+++ Day 20 & 21/snake_game/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 from snake import Snake
+from food import Food
 import time
 
 
@@ -11,6 +12,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(key="Up",fun=snake.up)
@@ -24,6 +26,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-    
+
+    if snake.segments[0].distance(food) < 15:
+        food.refresh()
+
 
 screen.exitonclick()
