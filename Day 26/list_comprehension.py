58359fdb489ea30e3ab9c2e85e4dd4816f82fb0d
Marija Cveevska <marijacveevska@gmail.com>
2026-02-03 21:44:17 +0100

    Day 26 - List and Dictionary comprehension

---
+++ Day 26/list_comprehension.py
@@ -0,0 +1,15 @@
+# Creating new list from an existing list 
+# FOR loop
+numbers = [1,2,3]
+new_list = []
+for n in numbers:
+    add_1 = n + 1
+    new_list.append(add_1)
+
+print(new_list)
+
+# List Comprehension
+# new_list = [new_item for item in list]
+numbers = [1,2,3]
+new_list = [n+1 for n in numbers]
+print(new_list)
