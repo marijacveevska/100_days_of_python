e7e2e9d519a20b80a882531b12f65d20ae64aba7
Marija Cveevska <marijacveevska@gmail.com>
2026-02-13 08:39:52 +0100

    Day 29 - Password Manager UI setup

--- Day 29/password-manager/main.py
+++ Day 29/password-manager/main.py
@@ -1,5 +1,17 @@
+import tkinter
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = tkinter.Tk()
+window.title("Password Manager")
+window.config(padx=20,pady=20)
+
+canvas = tkinter.Canvas(width = 200, height = 200)
+logo_img = tkinter.PhotoImage(file="Day 29/password-manager/logo.png")
+canvas.create_image(100,100,image=logo_img)
+canvas.pack()
+
+window.mainloop()
