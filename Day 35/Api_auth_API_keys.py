c176266dc2b7a8e968bf20ab52bd02ab5d08550e
Marija Cveevska <marijacveevska@gmail.com>
2026-03-15 09:40:39 +0100

    Day 35 - API auth,API keys and Rain alert message

---
+++ Day 35/Api_auth_API_keys.py
@@ -0,0 +1,14 @@
+"""
+
+API authentication is the process of verifying that a user or application is allowed to access an API.
+It ensures that only authorized clients can request data or perform actions through the API.
+APIs often use authentication to protect sensitive data and prevent misuse.
+One common method of API authentication is using an API key.
+An API key is a unique string of characters assigned to a specific user or application.
+The client includes this key in each request to identify itself to the server.
+The server checks the key to confirm the request comes from an approved source.
+If the key is valid, the API processes the request and returns the response.
+If the key is missing or invalid, the API usually rejects the request.
+API keys are simple to use but are often combined with other security methods for better protection.
+
+"""
---
+++ Day 35/rain_alert/main.py
@@ -0,0 +1,21 @@
+from data import OWM_Endpoint,api_key
+import requests
+
+weather_parameters = {"lat":52.370216,"lon":4.895168,"appid":api_key,"cnt":4}
+response = requests.get(url=OWM_Endpoint,params=weather_parameters)
+response.raise_for_status()
+data_results = response.json()["list"]
+
+print(data_results[0]["weather"][0]["id"])
+
+will_rain = False
+for hour_data in data_results:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+
+if will_rain:
+    print("Bring and umbrella")
+else:
+    print("All good today")
