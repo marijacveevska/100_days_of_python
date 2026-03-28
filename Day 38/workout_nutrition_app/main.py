512fb45144c7bbf1da4adf3a40a81c8b8beab7fe
Marija Cveevska <marijacveevska@gmail.com>
2026-03-28 09:27:50 +0100

    Day 38 - workout calorie app API

---
+++ Day 38/workout_nutrition_app/main.py
@@ -0,0 +1,24 @@
+from data import API_KEY, API_ID
+import requests
+from datetime import datetime
+
+nutrition_url = "https://app.100daysofpython.dev"
+
+nutrition_endpoint = f"{nutrition_url}/v1/nutrition/natural/exercise"
+nutri_parameters = { 
+    "query":"walked for 1 hour",
+    "weight_kg": 54,                  # Optional: Weight in kg (1-500)
+    "height_cm": 165,                 # Optional: Height in cm (1-300)
+    "age": 31,                        # Optional: Age (1-150)
+    "gender": "female"                # Optional: "male" or "female"
+
+}
+
+headers={ 
+    "Content-Type": "application/json",
+    "x-app-id": API_ID, 
+    "x-app-key": API_KEY
+}
+
+response = requests.post(url=nutrition_endpoint, json=nutri_parameters,headers=headers)
+print(response.text)
