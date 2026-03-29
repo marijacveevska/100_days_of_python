a4fd2fed2412dd64e97a0344bec53bced51cfd44
Marija Cveevska <marijacveevska@gmail.com>
2026-03-29 10:31:44 +0200

    Day 38 - workout and calorie calculation from nutrition API

--- Day 38/workout_nutrition_app/main.py
+++ Day 38/workout_nutrition_app/main.py
@@ -1,16 +1,19 @@
-from data import API_KEY, API_ID
+from data import API_KEY, API_ID, AGE,GENDER,WEIGHT_KG,HEIGHT_CM
 import requests
 from datetime import datetime
+
+
 
 nutrition_url = "https://app.100daysofpython.dev"
 
 nutrition_endpoint = f"{nutrition_url}/v1/nutrition/natural/exercise"
+
 nutri_parameters = { 
     "query":"walked for 1 hour",
-    "weight_kg": 65,                  # Optional: Weight in kg (1-500)
-    "height_cm": 175,                 # Optional: Height in cm (1-300)
-    "age": 50,                        # Optional: Age (1-150)
-    "gender": "female"                # Optional: "male" or "female"
+    "weight_kg": WEIGHT_KG,                  # Optional: Weight in kg (1-500)
+    "height_cm": HEIGHT_CM,                 # Optional: Height in cm (1-300)
+    "age": AGE,                        # Optional: Age (1-150)
+    "gender": GENDER               # Optional: "male" or "female"
 
 }
 
@@ -22,3 +25,20 @@ headers={
 
 response = requests.post(url=nutrition_endpoint, json=nutri_parameters,headers=headers)
 print(response.text)
+result = response.json()
+print(result)
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout":{
+    "date": today_date,
+    "time": now_time,
+    "exercise": exercise["name"].title(),
+    "duration": exercise["duration_min"],
+    "calories": exercise["nf_calories"]
+        }
+    }
+    print(f"These are the sheet inputs: {sheet_inputs}")
