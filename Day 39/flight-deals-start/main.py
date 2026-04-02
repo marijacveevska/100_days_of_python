2ade7d32d6352aeb6dfe5bf82a007d95348526b2
Marija Cveevska <marijacveevska@gmail.com>
2026-04-02 21:02:38 +0200

    Day 39 - sheety API and get data from google sheets

--- Day 39/flight-deals-start/main.py
+++ Day 39/flight-deals-start/main.py
@@ -3,4 +3,19 @@ from data_manager import DataManager
 from flight_data import FlightData
 from flight_search import FlightSearch
 from notification_manager import NotificationManager
+from data import API_GET, API_POST, API_PUT
+import requests
 
+endpoint = API_GET
+
+response = requests.get(url=endpoint)
+response.raise_for_status()
+data_results = response.json()
+sheet_data = data_results["prices"]
+
+for row in sheet_data:
+    if row["iataCode"] == "":
+        row["iataCode"] = "TESTING"
+
+print(sheet_data)
+#requests.post()
